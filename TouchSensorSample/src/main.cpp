7ebc9bd22843f4460b54c25cfea674477dff4f8d
puppypuppy2005 <3784814695@qq.com>
2024-04-08 18:38:03 +0800

    增加touch的逻辑，点亮led

--- TouchSensorSample/src/main.cpp
+++ TouchSensorSample/src/main.cpp
@@ -1,34 +1,23 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
-}
-
-void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
-}
-
+#define LED_PIN 13 // Define the LED pin
 #define TOUCH_PIN 2 // Define the touch pin
-
 void setup() {
   pinMode(TOUCH_PIN, INPUT); // Set the touch pin as input
+  pinMode(LED_PIN, OUTPUT); // Set the LED pin as output
   Serial.begin(9600); // Initialize serial communication
 }
 
+
 void loop() {
-  // put your main code here, to run repeatedly:
   int touchValue = digitalRead(TOUCH_PIN); // Read the touch sensor value
   if (touchValue == HIGH) {
     Serial.println("Touch detected!"); // Print a message if touch is detected
+    digitalWrite(LED_PIN, HIGH); // Turn on the LED
+  } else {
+    digitalWrite(LED_PIN, LOW); // Turn off the LED
   }
   delay(100); // Delay for 100 milliseconds
 }
+
