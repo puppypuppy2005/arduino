5886ddb397fc21f70accd1775f36bede7253fa87
puppypuppy2005 <165106002+puppypuppy2005@users.noreply.github.com>
2024-04-08 15:54:20 +0800

    first commit

---
+++ IRSensorSample/src/main.cpp
@@ -0,0 +1,29 @@
+#include <Arduino.h>
+//红外传感器示例
+// put function declarations here:
+int myFunction(int, int);
+int infraredPin = 7;
+int ledPin = 8;
+
+void setup() {
+  pinMode(infraredPin, INPUT);
+  pinMode(ledPin, OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  int infraredData = digitalRead(infraredPin);
+  
+  if (infraredData == LOW) {
+    digitalWrite(ledPin, HIGH);
+  } else {
+    digitalWrite(ledPin, LOW);
+  }
+  Serial.println(infraredData);
+  // delay(1000);
+}
+
+// put function definitions here:
+int myFunction(int x, int y) {
+  return x + y;
+}
